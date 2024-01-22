e6d54f0c4a2b640d1d6d829861f2e8875921b2e9
kavinduUdhara <wmkavinduudhara@gmail.com>
2024-01-22 15:21:41 +0530

    added files of basic print statements on c with comparing them to to java

---
+++ hello_world.java
@@ -0,0 +1,13 @@
+public class hello_world {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("this is the 2nd line\n");
+
+        int myNum = 10;
+        int myNum1;
+        myNum1 = 15;
+        System.out.println("The value of myNum is: " + myNum);
+        System.out.println(String.format("The value of myNum1 is: %d", myNum1));
+        System.out.printf("The sum of myNum and myNum1 is: %d", myNum + myNum1);
+    }
+}

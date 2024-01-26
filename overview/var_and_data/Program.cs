ebb7ce4362c82b25eb125123ed8b3870824f62dc
kavinduUdhara <wmkavinduudhara@gmail.com>
2024-01-27 00:38:22 +0530

    renamed folder helloWorld to hello_world

--- overview/var_and_data/Program.cs
+++ overview/var_and_data/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//basic value type varibles
+using System.Numerics;
+using System.Security.Principal;
+
+int i = 10;
+float f = 10.5f;
+decimal d = 10.0m;
+bool b = true;
+char c = 'c';
+
+//string type
+string str = "this is a string";
+
+//implicit variable (that system decide what the data type is)
+//according to the assigned value
+var x = 10;
+var z = "Hello!";
+
+//array
+int[] vals = new int[5];
+string[] strs = {"one", "two", "three", "four"};
+
+//print above var. using a formatting string
+Console.WriteLine("Printing values assigned: ");
+Console.WriteLine("{0},{1},{2},{3},{4},{5},{6},{7}",i,f,d,b,c,str,x,z);
+
+//null means "no value"
+Console.WriteLine("\nFollowing will print a null value(will show up as empty string): ");
+object o = null;
+Console.WriteLine(o);
+
+//implicit conversion between types
+long bignum;
+bignum = i;

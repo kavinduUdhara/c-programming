f0b38b11a6adfe0b8406346de6d26c27e7de3176
kavinduUdhara <wmkavinduudhara@gmail.com>
2024-01-29 13:08:32 +0530

    completed: comments

---
+++ overview/comments/Program.cs
@@ -0,0 +1,7 @@
+﻿///XML comments are used to halp provide documentations
+///<summery>
+///THis is example of comments
+///</summery>
+//this is a signle line comment
+/*this is a
+multi line comment*/

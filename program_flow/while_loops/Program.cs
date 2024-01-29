b22906aeda29b614c968b28b3eda6a223b8b8bb7
kavinduUdhara <wmkavinduudhara@gmail.com>
2024-01-29 15:09:43 +0530

    completed while_loops

---
+++ program_flow/while_loops/Program.cs
@@ -0,0 +1,14 @@
+﻿string str = "";
+Console.WriteLine("this is while loop");
+while (str != "exit"){
+    Console.Write("enter value for str: ");
+    str = Console.ReadLine();
+    Console.WriteLine("you entered: {0}", str);
+}
+
+Console.WriteLine("this is do whole loop");
+do{
+    Console.Write("enter value for str: ");
+    str = Console.ReadLine();
+    Console.WriteLine("you entered: {0}",str);
+} while(str != "exit");

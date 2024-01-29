eaccf28cff9eab03867e15e431eaa7280a6240e2
kavinduUdhara <wmkavinduudhara@gmail.com>
2024-01-29 15:40:08 +0530

    completed: exceptions

---
+++ program_flow/exceptions/Program.cs
@@ -0,0 +1,18 @@
+﻿int x = 10000;
+int y = 10;
+int z;
+
+try{
+    if (x > 1000){
+        throw new ArgumentOutOfRangeException("x","x has to be 1000 or less");
+    }
+    z = x/y;
+    Console.WriteLine(z);
+} catch (DivideByZeroException e){
+    Console.WriteLine("opps! something went wrong");
+    Console.WriteLine(e.Message);
+} catch (ArgumentOutOfRangeException e){
+    Console.WriteLine(e.Message);
+} finally{
+    Console.WriteLine("This line always runs");
+}

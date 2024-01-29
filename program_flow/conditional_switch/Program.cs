b7eecf99a2d776cc0d9073203af976467e686bc2
kavinduUdhara <wmkavinduudhara@gmail.com>
2024-01-29 14:44:20 +0530

    completed conditional-switch

---
+++ program_flow/conditional_switch/Program.cs
@@ -0,0 +1,21 @@
+﻿int theVal = 70;
+
+switch(theVal){
+    case 50:
+        Console.WriteLine("theVal is 50");
+        break;
+    case 51:
+        Console.WriteLine("theVal is 51");
+        break;
+    case 52:
+    case 53:
+    case 54:
+        Console.WriteLine("theVal is in between 52 and 54");
+        break;
+    case <50:
+        Console.WriteLine("theVal is lower than 50");
+        break;
+    default:
+        Console.WriteLine("theval is greater than 54");
+        break;
+}

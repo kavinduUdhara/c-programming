e86c8c917d884c9ac757867178f92999559d1070
kavinduUdhara <wmkavinduudhara@gmail.com>
2024-01-29 15:29:57 +0530

    completed break_continue

---
+++ program_flow/break_continue/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] values = {15, 7, 12, 23, 41, 28, 9, 17, 36};
+
+foreach (int value in values){
+    if (value <= 20){
+        continue;
+    }
+    Console.WriteLine($"the value is: {value}");
+    if (value >= 40){
+        break;
+    }
+}

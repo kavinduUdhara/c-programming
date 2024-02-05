55b554b2b6dc071be34d4b2a430ad80c5f2cc9e9
kavinduUdhara <wmkavinduudhara@gmail.com>
2024-02-06 00:30:47 +0530

    completed using_stringduilder

---
+++ strings/using_stringBuilder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+int jumpCount = 10;
+string[] animals = {"goats", "cats", "pigs"};
+
+//create a string builder
+StringBuilder sb = new StringBuilder();
+
+Console.WriteLine($"Capacity: {sb.Capacity} | length: {sb.Length}");
+
+sb.Append("The quick brown fox ");
+sb.Append("jumped over the lazy dog.");
+sb.AppendLine();
+sb.AppendFormat($"He did this {jumpCount} times.");
+sb.AppendLine();
+
+sb.Append("He also jumped over ");
+sb.AppendJoin(", ", animals);
+
+sb.Replace("fox", "cat");
+sb.Insert(0, "This is the ");
+
+Console.WriteLine($"Capacity: {sb.Capacity} | length: {sb.Length}");
+Console.WriteLine(sb.ToString());
